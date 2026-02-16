6361a5a59d3373f908caf0b14e3f4e2468e8a2da
sylvesterdsz <sylvester.d@gmail.com>
2026-02-16 14:18:38 +1100

    Exercise 1.2

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -6,18 +6,42 @@ const Header = (props) => {
   );
 };
 
-const Content = (props) => {
+const Part1 = (props) => {
   return (
     <div>
       <p>
         {props.part1} {props.exercises1}
       </p>
+    </div>
+  );
+};
+
+const Part2 = (props) => {
+  return (
+    <div>
       <p>
         {props.part2} {props.exercises2}
       </p>
+    </div>
+  );
+};
+
+const Part3 = (props) => {
+  return (
+    <div>
       <p>
         {props.part3} {props.exercises3}
       </p>
+    </div>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part1 part1={props.part1} exercises1={props.exercises1} />
+      <Part2 part2={props.part2} exercises2={props.exercises2} />
+      <Part3 part3={props.part3} exercises3={props.exercises3} />
     </div>
   );
 };
