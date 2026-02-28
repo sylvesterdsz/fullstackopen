470f386abe6d6ae1fae871faa53ab293d8195c00
sylvesterdsz <sylvester.d@gmail.com>
2026-02-28 23:11:39 +1100

    Exercise 2.2

--- part2/src/components/Course.jsx
+++ part2/src/components/Course.jsx
@@ -14,11 +14,17 @@ const Part = ({ part }) => (
   </p>
 );
 
+const Total = (props) => <h3>total of {props.total} exercises</h3>;
+
 const Course = ({ course }) => {
+  const calculatedTotal = course.parts
+    .map((p) => p.exercises)
+    .reduce((sum, exercises) => sum + exercises, 0);
   return (
     <div>
       <Header course={course.name} />
       <Content parts={course.parts} />
+      <Total total={calculatedTotal} />
     </div>
   );
 };
