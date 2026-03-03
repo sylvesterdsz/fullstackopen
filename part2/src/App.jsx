da44d44665cd6db828b88b576618404107f41fa2
sylvesterdsz <sylvester.d@gmail.com>
2026-03-03 23:52:01 +1100

    Exercise 2.6

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,59 +1,38 @@
-import Course from "./components/Course";
+import { useState } from "react";
 
 const App = () => {
-  const courses = [
-    {
-      name: "Half Stack application development",
-      id: 1,
-      parts: [
-        {
-          name: "Fundamentals of React",
-          exercises: 10,
-          id: 1,
-        },
-        {
-          name: "Using props to pass data",
-          exercises: 7,
-          id: 2,
-        },
-        {
-          name: "State of a component",
-          exercises: 14,
-          id: 3,
-        },
-        {
-          name: "Redux",
-          exercises: 11,
-          id: 4,
-        },
-      ],
-    },
-    {
-      name: "Node.js",
-      id: 2,
-      parts: [
-        {
-          name: "Routing",
-          exercises: 3,
-          id: 1,
-        },
-        {
-          name: "Middlewares",
-          exercises: 7,
-          id: 2,
-        },
-      ],
-    },
-  ];
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const addNumber = (event) => {
+    event.preventDefault();
+    const personObj = {
+      name: newName,
+    };
+    setPersons(persons.concat(personObj));
+  };
+
+  const handleNumberChange = (event) => {
+    setNewName(event.target.value);
+  };
 
   return (
     <div>
-      <h1>Web development curriculum</h1>
-      {courses.map((course) => (
-        //i have already separated the course component and made it a separate module
-        //adding this comment to commit for exercise 2.5
-        <Course key={course.id} course={course} />
-      ))}
+      <h2>Phonebook</h2>
+      <form onSubmit={addNumber}>
+        <div>
+          name: <input value={newName} onChange={handleNumberChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((p) => (
+          <li key={p.name}>{p.name}</li>
+        ))}
+      </ul>
     </div>
   );
 };
