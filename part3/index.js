401036467d8822fbeb1965f7eee8df4908ff0291
sylvesterdsz <sylvester.d@gmail.com>
2026-03-10 22:54:20 +1100

    Exercise 3.7

--- part3/index.js
+++ part3/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
+
 app.use(express.json())
+app.use(morgan("tiny"))
 
 let persons = [
     { 
@@ -88,6 +91,12 @@ app.post("/api/persons", (request,response) => {
     persons = persons.concat(person)
     response.json(person)
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
 
 
 const PORT = 3001
