842954c2631b2b2b1316c52e318ee7bc4956804a
sylvesterdsz <sylvester.d@gmail.com>
2026-03-11 23:09:10 +1100

    Exercise 3.10

--- part3/index.js
+++ part3/index.js
@@ -132,8 +132,9 @@ const unknownEndpoint = (request, response) => {
 app.use(unknownEndpoint)
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
+
 app.listen(PORT, () => {
-    console.log("Server is running and listening for requests")
+  console.log(`Server running on port ${PORT}`)
 })
 
