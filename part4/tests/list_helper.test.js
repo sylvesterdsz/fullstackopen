623af2edaf40ead173eb960ba3e50a5c35e0f397
sylvesterdsz <sylvester.d@gmail.com>
2026-03-17 20:57:09 +1100

    Exercise 4.5

--- part4/tests/list_helper.test.js
+++ part4/tests/list_helper.test.js
@@ -59,3 +59,82 @@ describe('total likes', () => {
     assert.strictEqual(totalLikes, 14)
   })
 })
+
+describe('favorite blog', () => {
+  test('returns null for an empty list', () => {
+    const blogs = []
+    assert.deepStrictEqual(listHelper.favoriteBlog(blogs), null)
+  })
+  test('returns only one blog if the list has only one item', () => {
+    const blogs = [
+      {
+        title: 'Book title',
+        author: 'Sylvester Dsouza',
+        url: 'http://localhost',
+        likes: 5,
+      },
+    ]
+    const favoriteBlog = listHelper.favoriteBlog(blogs)
+    assert.deepStrictEqual(blogs[0], favoriteBlog)
+  })
+  test('returns the blog with the most likes', () => {
+    const blogs = [
+      {
+        title: 'Book title 1',
+        author: 'Sylvester Dsouza',
+        url: 'http://localhost',
+        likes: 5,
+      },
+      {
+        title: 'Book title 2',
+        author: 'Sylvester Dsouza',
+        url: 'http://localhost',
+        likes: 6,
+      },
+      {
+        title: 'Book title 3',
+        author: 'Sylvester Dsouza',
+        url: 'http://localhost',
+        likes: 2,
+      },
+      {
+        title: 'Book title 4',
+        author: 'Sylvester Dsouza',
+        url: 'http://localhost',
+        likes: 1,
+      },
+    ]
+    const favoriteBlog = listHelper.favoriteBlog(blogs)
+    assert.deepStrictEqual(favoriteBlog, blogs[1])
+  })
+  test('handles multiple blogs with most likes', () => {
+    const blogs = [
+      {
+        title: 'Book title 1',
+        author: 'Sylvester Dsouza',
+        url: 'http://localhost',
+        likes: 5,
+      },
+      {
+        title: 'Book title 2',
+        author: 'Sylvester Dsouza',
+        url: 'http://localhost',
+        likes: 6,
+      },
+      {
+        title: 'Book title 3',
+        author: 'Sylvester Dsouza',
+        url: 'http://localhost',
+        likes: 6,
+      },
+      {
+        title: 'Book title 4',
+        author: 'Sylvester Dsouza',
+        url: 'http://localhost',
+        likes: 1,
+      },
+    ]
+    const favoriteBlog = listHelper.favoriteBlog(blogs)
+    assert.deepStrictEqual(favoriteBlog, blogs[1])
+  })
+})
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -6,7 +6,16 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blogs) => blogs.likes + sum, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return null
+
+  return blogs.reduce((favorite, blog) =>
+    blog.likes > favorite.likes ? blog : favorite,
+  )
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 }
