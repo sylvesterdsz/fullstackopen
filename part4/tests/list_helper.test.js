5e8d748c06064ea400590db1e7a70301c54cf702
sylvesterdsz <sylvester.d@gmail.com>
2026-03-17 20:32:57 +1100

    Exercise 4.4

--- part4/tests/list_helper.test.js
+++ part4/tests/list_helper.test.js
@@ -1,4 +1,4 @@
-const { test } = require('node:test')
+const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 
@@ -6,4 +6,56 @@ test('dummy returns 1', () => {
   const blogs = []
   const dummyValue = listHelper.dummy(blogs)
   assert.strictEqual(dummyValue, 1)
+})
+
+describe('total likes', () => {
+  test('of empty list is zero', () => {
+    const blogs = []
+    const totalLikes = listHelper.totalLikes(blogs)
+    assert.strictEqual(totalLikes, 0)
+  })
+
+  test('when list has only one blog equals the likes of that', () => {
+    const blogs = [
+      {
+        title: 'Book title',
+        author: 'Sylvester Dsouza',
+        url: 'http://localhost',
+        likes: 5,
+      },
+    ]
+    const totalLikes = listHelper.totalLikes(blogs)
+    assert.strictEqual(totalLikes, 5)
+  })
+
+  test('of a bigger list is calculated right', () => {
+    const blogs = [
+      {
+        title: 'Book title 1',
+        author: 'Sylvester Dsouza',
+        url: 'http://localhost',
+        likes: 5,
+      },
+      {
+        title: 'Book title 2',
+        author: 'Sylvester Dsouza',
+        url: 'http://localhost',
+        likes: 6,
+      },
+      {
+        title: 'Book title 3',
+        author: 'Sylvester Dsouza',
+        url: 'http://localhost',
+        likes: 2,
+      },
+      {
+        title: 'Book title 4',
+        author: 'Sylvester Dsouza',
+        url: 'http://localhost',
+        likes: 1,
+      },
+    ]
+    const totalLikes = listHelper.totalLikes(blogs)
+    assert.strictEqual(totalLikes, 14)
+  })
 })
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,6 +2,11 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, blogs) => blogs.likes + sum, 0)
+}
+
 module.exports = {
   dummy,
+  totalLikes,
 }
